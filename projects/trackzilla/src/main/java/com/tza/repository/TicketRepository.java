0dd443b79327d81be047447e96bed874122ef365
Julian Carachure <jcarachu@outlook.com>
2021-05-01 20:54:52 -0700

    [DELETE] remove duplicate directory

--- projects/trackzilla/src/main/java/com/tza/repository/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tza.repository;
-
-import com.tza.entity.Ticket;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TicketRepository extends CrudRepository<Ticket, Integer> {
-}
