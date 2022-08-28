f2d2142b59cedb43c5213a83cd02efb7f6269dc5
Julian Carachure <thegreatjoules@outlook.com>
2022-08-27 23:32:44 -0700

    [UPDATE] Refactored directories with challenges and interviews

--- Interview/Other/leetcode/packageEx/utils/ShowSystem.java
+++
@@ -1,9 +0,0 @@
-package tup.utils;
-public class ShowSystem
-{
-	public void show()
-	{
-		System.out.println(System.getProperty("os.name"));
-		System.out.println(System.getProperty("os.version"));
-	}
-}
